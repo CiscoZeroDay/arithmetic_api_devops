f10896500c954f9a4e50bd3a31905b4c02297ba3
Dracarys261 <jhonnyrazaf01@gmail.com>
2025-10-18 14:12:35 +0100

    Update app.py to disable debug mode and fix git_watch_and_deploy.sh repository path

--- git_watch_and_deploy.sh
+++ git_watch_and_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPO_PATH="https://github.com/CiscoZeroDay/arithmetic_api_devops"
+REPO_PATH="E:/PROJECT/Devsecops/Lab1/arithmetic"
 LAST_COMMIT=$(git -C $REPO_PATH rev-parse HEAD)
 
 while true; do
@@ -9,7 +9,9 @@ while true; do
     if [ "$LAST_COMMIT" != "$NEW_COMMIT" ]; then
         echo "New commit detected. Pulling changes..."
         git -C $REPO_PATH pull
-        docker build -t simple-api .
+        echo "Rebuilding Docker image..."
+        docker build -t simple-api $REPO_PATH
+        echo "Redeploying container..."
         docker stop simple-api-container 2>/dev/null
         docker rm simple-api-container 2>/dev/null
         docker run -d --name simple-api-container -p 5000:5000 simple-api
